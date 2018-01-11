4e5a533e2190726f7e816e35391a94a42c4781cd
sonalparab <sparab@stuy.edu>
2018-01-10 19:28:19 -0500

    Colored textbox

--- static/js/game.js
+++ static/js/game.js
@@ -17,6 +17,9 @@ var start;
 var t = 0;
 var typedPrompt = '';
 var leftPrompt = prompt;
+//l is used to keep track if a list was made of the letters in the current word
+var l = 0;
+var list = [];
 
 
 var toCheck = function() {
@@ -26,6 +29,7 @@ var toCheck = function() {
     if (n === promptNumWords.length) {
 	n = -1;
 	t = 0;
+	l = 0;
     }
 
     if (n >= 0) {
@@ -45,6 +49,7 @@ var toCheck = function() {
 	typing.innerHTML = leftPrompt.replace(promptNumWords[n], "<div style=\"color: hotpink; display: inline; \">" + promptNumWords[n] + "</div>");
 	
     }
+    //if the prompt was successfully typed, calculate the words per minute
     else if(n === -1){
       var elapsed = (new Date().getTime() - start) / 1000;
       var wpm = Math.floor((prompt.length/5) / (elapsed/60));
@@ -54,13 +59,50 @@ var toCheck = function() {
     else{
         document.getElementById('wordUpTo').innerHTML = "Finished!";
     }
-    //if textbox matches the word, it clears the textbox, raises the word array counter
-    if ((promptNumWords[n] + " ") === inputThing){
-      document.getElementById('inputText').value = "";
+
+    //make a list of all the characters in the current word
+    // use l to make the list only when the current word changes
+    if(l === 0){
+	var word = promptNumWords[n];
+	var i = 0;
+	list = [];
+	for(i = 0; i < word.length; i++){
+	    list.push(word.charAt(i));
+	}
+	//update l because the list was made
+	l = 1;
+    }	
+    
+    //if textbox matches the word, clear the textbox, raise the word array counter
+   if ((promptNumWords[n] + " ") === inputThing){
+        document.getElementById('inputText').value = "";
 	n++;
 	t = 1;
+	l = 0;
       console.log("yay!");
       return true;
+    }
+    //if the textbox does not match the word, check if the current progress is correct
+    else{
+	var j = 0;
+	var correct = true;
+	//check if the characters inputed so far match the corresponding
+	// characters in the current word
+	for(j = 0; j < inputThing.length; j++){
+	    if(inputThing.charAt(j) != list[j]){
+		correct = false;
+	    }
+	}
+	
+	var textbox = document.getElementById('inputText');
+	//if an incorrect character was typed, make the textbox red
+	if(!correct){
+	    textbox.style.backgroundColor = "red";
+	}
+	//if the characters so far are correct, make the textbox white
+	else{
+	    textbox.style.backgroundColor = "white";
+	}
     }
 };
 
